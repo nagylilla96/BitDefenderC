e874ee801390acad1ca10fbf880b6102b31a1b98
nagylilla96 <n.lilla@yahoo.com>
2017-01-14 21:22:53 +0200

    Started writing tester for arrayvector

---
+++ ArrayVectorTester/cmake-build-debug/CMakeFiles/ArrayVectorTester.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ArrayVectorTester.dir/main.c.o"
+  "ArrayVectorTester.pdb"
+  "ArrayVectorTester"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ArrayVectorTester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ArrayVectorTester/cmake-build-debug/CMakeFiles/ArrayVectorTester.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ArrayVectorTester.
+# This may be replaced when dependencies are built.
